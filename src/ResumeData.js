3430878a2f60d08ba9857a479ef845553cc09f4e
Leonardo Miscevic <leonardomiscevic@gmail.com>
2025-05-06 22:59:51 +0200

    data update

--- src/ResumeData.js
+++ src/ResumeData.js
@@ -8,29 +8,38 @@ export const ResumeData = {
   ],
   education: [
     {
-      title: "Master's in Information Sciences",
-      college: "Faculty of Philosophy",
-      dateOfCompletion: "Ongoing",
-      address: "Matice hrvatske b.b.",
-      city: "88000, Mostar",
-      country: "Bosnia and Herzegovina",
-    },
-    {
       title: "Bachelor's in Information Sciences",
       college: "Faculty of Philosophy",
-      dateOfCompletion: "October 2024",
+      dateOfCompletion: "May 2024",
       address: "Matice hrvatske b.b.",
       city: "88000, Mostar",
       country: "Bosnia and Herzegovina",
     },
     {
-      title: "Barmen",
-      college: "Bar akademija",
-      dateOfCompletion: "October 2020",
-      address: "Radoja Dakića bb",
-      city: "88000, Mostar",
-      country: "Bosnia and Herzegovina",
-    },
+    title: "Hotel and tourism technician",
+    college: "Tourism and Hospitality School",
+    dateOfCompletion: "Jun 2019",
+    address: "Kralja Tvrtka 3",
+    city: "Mostar, 88000",
+    country: "Bosnia & Herzegovina",
+  },
+  {
+    title: "Waiter",
+    college: "Tourism and Hospitality School",
+    dateOfCompletion: "Jun 2016",
+    address: "Kralja Tvrtka 3",
+    city: "Mostar, 88000",
+    country: "Bosnia & Herzegovina",
+  },
+  
+  {
+    title: "Barmen",
+    college: "Bar akademija",
+    dateOfCompletion: "October 2020",
+    address: "Radoja Dakića bb",
+    city: "88000, Mostar",
+    country: "Bosnia and Herzegovina",
+  },
   ],
   languages: [
     { id: 1, title: "Croatian" },
@@ -48,7 +57,7 @@ export const ResumeData = {
       title: "Personal Portfolio",
       date: "2024",
       description: "Personal Portfolio",
-      link: "https://github.com/LeonardoMiscevic/Portfolio",
+      link: "https://github.com/CookieCookieAlex/Resume",
     },
   ],
   workExperience: [
@@ -70,6 +79,58 @@ export const ResumeData = {
       description:
         "During the Work and Travel program in the USA, I worked in multiple roles, including food runner, busser, and host. I honed my communication skills, working in an international team and adapting to different cultural expectations of guests.",
     },
+    {
+      title: "Server",
+      company: "Pizzeria Marko",
+      city: "Stari Grad, Hvar",
+      country: "Croatia",
+      dateOfCompletion: "May 2022 - Oct 2022",
+      description:
+        "As a shift manager, I organized teams, supervised inventory and daily operations. This role enabled me to develop leadership skills and problem-solving abilities in a dynamic environment.",
+    },
+    {
+      title: "Bartender",
+      company: "Stewmans lobster pound",
+      city: "Hvar",
+      country: "Croatia",
+      dateOfCompletion: "Jun 2021 - Oct 2021",
+      description:
+        "I worked as a bartender at a luxury beach club, specializing in preparing cocktails and luxury food.",
+    },
+    {
+      title: "Server",
+      company: "Pizzeria Marko",
+      city: "Hvar",
+      country: "Croatia",
+      dateOfCompletion: "Jun 2019 - Oct 2020",
+      description:
+        "As the head waiter, I led a team of waiters and was responsible for coordinating the work of the kitchen and serving. We served pizzas, meat dishes and fish, with a special emphasis on the presentation of wine and drinks. I gained strong organizational skills and the ability to lead a team. I worked for two seasons.",
+    },
+    {
+      title:"Server",
+      company:"Jurin Podrum",
+      city:"Stari Grad, Hvar",
+      country:"Croatia",
+      dateOfCompletion:"April 2018 - Oct 2018",
+      description:" I worked in a family restaurant, where I gained experience serving local food and drinks. Although there was no professional advancement in this job, I gained additional experience working with guests in a traditional setting.",
+    },
+    {
+      title:"Server",
+      company:"Simple",
+      city:"Stari Grad, Hvar",
+      country:"Croatia",
+      dateOfCompletion:"April 2017 - Oct 2017",
+      description:"In this bistro, I worked as a server and bartender in shifts with a colleague. The focus was on serving breakfast and fast food menus quickly and efficiently. I learned how to work under pressure and develop multitasking skills in a busy environment.",
+    },
+    {
+      title:"Server",
+      company:"Pinetta",
+      city:"Stari Grad, Hvar",
+      country:"Croatia",
+      dateOfCompletion:"July 2016 - Oct 2016",
+      description:"I started as a bartender and soon took on the role of head waiter. I was responsible for the professional presentation of Hvar wines and serving seafood specialties. I gained skills in quality service, managing stressful situations and maintaining high standards of service.",
+    },
+
     // Add the remaining work experiences here...
   ],
   contact: [
